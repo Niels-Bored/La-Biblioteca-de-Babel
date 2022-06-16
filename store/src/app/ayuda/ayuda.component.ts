5198d34d6237eec208cf7e0e802fa22908563920
anaSofiaING <104743054+anaSofiaING@users.noreply.github.com>
2022-06-16 09:57:50 -0500

    Cambios en el select

--- store/src/app/ayuda/ayuda.component.ts
+++ store/src/app/ayuda/ayuda.component.ts
@@ -1,46 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AccesibilidadService } from '../accesibilidad.service';
 @Component({
   selector: 'app-ayuda',
   templateUrl: './ayuda.component.html',
   styleUrls: ['./ayuda.component.css']
 })
 export class AyudaComponent implements OnInit {
-  tamano: number = 0;
-  fondo: string = "";
-  color: string = "";
-  b_color: string = "";
-  bootstrap: string = "";
-  presentacion = {
-    'background-color': this.fondo,
-    'color': this.color,
-    'font-size.px': this.tamano,
-  };
-  icono: string = "";
-  img0: string = "";
-  img1: string = "";
-  img2: string = "";
+  constructor(public accesib: AccesibilidadService){
 
+  }
+  
   ngOnInit(): void {
-  }
-  cambiarFondo(e:any) {
-    switch(e.target.value){
-      case "Escala de Grises":
-        this.presentacion['background-color'] = "grey";
-        this.presentacion['color'] = "black";
-        break;
-      case "Normal":
-        this.presentacion['background-color'] = "pink";
-        this.presentacion['color'] = "black";
-        break;
-      case "Alto Contraste":
-        this.presentacion['background-color'] = "black";
-        this.presentacion['color'] = "blue";
-        break;
-    } 
-
-  }
-  cambiarTamano(e:any) {
-    this.presentacion['font-size.px'] = e.target.value;
   }
 }
