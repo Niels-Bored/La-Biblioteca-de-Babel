38a0383014539c45345c17c554cde5e4c4fa1665
AlbertoSnchez <alberto.sanchez966@gmail.com>
2022-06-16 23:55:51 -0500

    Revert "Merge branch 'main' of https://github.com/Niels-Bored/La-Biblioteca-de-Babel into main"
    This reverts commit c60a2786200be007cc7c2c5d7de38c66c44ec22e, reversing
    changes made to afd398beb466f31dc4fd45b7750992b28712b564.

--- store/src/app/analytics/analytics.component.ts
+++ store/src/app/analytics/analytics.component.ts
@@ -12,111 +12,18 @@ export class AnalyticsComponent implements OnInit {
 
   libros : any[] = [];
   descargas : any[] = [];
-  descargasaux : any[] = [];
-  orden : any[] = [];
   nombres: any[] = [];
-  gen:any[] = [];
-
-  topD:any[] = [];
-  topN:any[] = [];
+  generos: any[] = [];
+  numGen: number[] = [];
   
-
-  generos: any[] = ["Dark Humor","Fantasy","Romance","Science Fiction","Terror","Thriller","Other"];
-  numGen: number[] = [0,0,0,0,0,0,0];  
 
   constructor(public firebase:FirebaseService, public router:Router) { 
     this.firebase.recuperar().subscribe((res: any) => {
       this.libros = res;
       console.log(this.libros);
     });
-    setTimeout(() => {  
-      this.generaGraf();
-    }, 4000);
   }
   ngOnInit(): void {
-    
-  }
-
-  generaGraf(){
-    for (let i = 0; i < this.libros.length; i++) {
-      this.descargas[i] = parseInt(this.libros[i]['Descargas']);
-      this.descargasaux[i] = this.descargas[i];
-      this.nombres[i] = this.libros[i]['Titulo'];
-      this.gen[i] = this.libros[i]['Genero'];
-    }
-    this.descargasaux.sort((a,b)=>a-b);
-    var x = 0;
-    for (let index = this.descargas.length-1; index >= this.descargas.length-10; index--) {
-      this.orden[x++] =  this.descargas.indexOf(this.descargasaux[index])
-    }
-    for (let j = 0; j < 10; j++) {
-      this.topD[j] = this.descargas[this.orden[j]];
-      this.topN[j] = this.nombres[this.orden[j]] 
-    }
-
-    const graficatop = new Chart("top10", {
-      type: 'bar',
-      data:{
-        labels:this.topN,
-        datasets:[{
-          label: 'Descargas',
-          data: this.topD
-        }]
-      },
-      options: {
-        scales:{
-          y:{
-            beginAtZero:true
-          }
-        },
-        responsive:true
-      }
-    });
-
-    for (let index = 0; index < this.libros.length; index++) {
-      switch (this.gen[index]) {
-        case 'Dark Humor':
-          this.numGen[0]++;
-          break;
-        case 'Fantasy':
-          this.numGen[1]++;
-          break;
-        case 'Romance':
-          this.numGen[2]++;
-          break;
-        case 'Science Fiction':
-          this.numGen[3]++;
-          break;
-        case 'Terror':
-          this.numGen[4]++;
-          break;
-        case 'Thriller':
-          this.numGen[5]++;
-          break;
-        default:
-          this.numGen[6]++;
-          break;
-      }
-    }
-    
-    const graficat = new Chart("topCat", {
-      type: 'pie',
-      data:{
-        labels:this.generos,
-        datasets:[{
-          label: 'Descargas',
-          data: this.numGen
-        }]
-      },
-      options: {
-        scales:{
-          y:{
-            beginAtZero:true
-          }
-        },
-        responsive:true
-      }
-    });
     
   }
 
--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirebaseService } from './services/firebase.service';
@@ -45,16 +45,15 @@ import { LibroComponent } from './libro/libro.component';
     LibroComponent,
   ],
   imports: [
-    CommonModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     QRCodeModule,
+    NgChartsModule,
     NgxSpinnerModule,
-    BrowserAnimationsModule,
-    NgChartsModule
+    BrowserAnimationsModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true}
